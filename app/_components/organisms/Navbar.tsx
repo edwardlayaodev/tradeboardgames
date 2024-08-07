ade105d948a9ab0dd3134953af386e9c80f130aa
Edward Layao <edward.layao.dev@proton.me>
2024-08-07 13:45:50 +0800

    create footer and navbar components

---
+++ app/_components/organisms/Navbar.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import { createClient } from "@/app/_utils/supabase/server";
+import { Atom } from "../atoms";
+
+export default async function Navbar() {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  return (
+    <header className="bg-base-100 px-4">
+      <div className="navbar max-w-[1280px] mx-auto">
+        <div className="navbar-start">
+          <Link
+            className="flex flex-row justify-center items-center gap-2"
+            href={"/"}
+          >
+            <Atom.Brand
+              logoProps={{
+                src: "/dice.svg",
+                width: 32,
+                height: 32,
+                alt: "dice-logo",
+              }}
+              label={
+                <span className="text-secondary">
+                  <span className="text-red-600">trade</span>board
+                  <span className="text-blue-600">games</span>
+                </span>
+              }
+            />
+          </Link>
+        </div>
+
+        <div className="navbar-end">
+          {!user && (
+            <Link href={"/sign-in"} className="btn btn-primary">
+              Sign-In
+            </Link>
+          )}
+          {user && (
+            <Link href={"/dashboard/user"} className="btn btn-primary">
+              My Account
+            </Link>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+}
